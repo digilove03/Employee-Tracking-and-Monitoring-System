33c266d6847453a222ef12fbdb1eedc9f8c3e763
digilove03 <142278349+digilove03@users.noreply.github.com>
2025-02-04 16:15:53 +0800

    System Database

---
+++ MONITORING SYSTEM/emts.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 04, 2025 at 09:12 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `emts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(4) NOT NULL,
+  `username` int(50) NOT NULL,
+  `password` int(50) NOT NULL,
+  `email` int(100) NOT NULL,
+  `department` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(8) NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `middle_name` varchar(200) NOT NULL,
+  `sex` enum('Female','Male') NOT NULL,
+  `birthdate` date NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `department` varchar(200) NOT NULL,
+  `civil_status` enum('Single','Married','Divorced','Seperated','Widowed') NOT NULL,
+  `hiring_date` date NOT NULL,
+  `contact_number` int(11) NOT NULL,
+  `email_address` varchar(200) NOT NULL,
+  `status` enum('Working','On Break','On Leave','Available') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_status_log`
+--
+
+CREATE TABLE `employee_status_log` (
+  `id` int(16) NOT NULL,
+  `employee_id` int(8) NOT NULL,
+  `status` enum('Working','On Break','On Leave','Available') NOT NULL,
+  `start_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `end_time` timestamp NULL DEFAULT current_timestamp(),
+  `duration` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(12) NOT NULL,
+  `employee_id` int(8) NOT NULL,
+  `record_number` varchar(16) NOT NULL,
+  `date_generated` timestamp NOT NULL DEFAULT current_timestamp(),
+  `task_group` varchar(75) NOT NULL,
+  `total_tasks` int(11) NOT NULL,
+  `completed_tasks` int(11) NOT NULL,
+  `ongoing_tasks` int(11) NOT NULL,
+  `cancelled_tasks` int(11) NOT NULL,
+  `average_duration` int(11) NOT NULL,
+  `remarks` int(11) NOT NULL,
+  `efficiency_score` float NOT NULL,
+  `employee_performance` enum('Outstanding','Very Satisfactory','Satisfactory','Fair','Poor') NOT NULL,
+  `delay` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `record_number` varchar(16) NOT NULL,
+  `employee_id` int(8) NOT NULL,
+  `service` varchar(200) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `service_status` enum('Ongoing','Completed','Cancelled') NOT NULL,
+  `time_started` timestamp NOT NULL DEFAULT current_timestamp(),
+  `time_ended` timestamp NULL DEFAULT current_timestamp(),
+  `deadline` datetime DEFAULT NULL,
+  `completion_time` int(11) DEFAULT NULL,
+  `remarks` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`) KEY_BLOCK_SIZE=4;
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`) KEY_BLOCK_SIZE=8;
+
+--
+-- Indexes for table `employee_status_log`
+--
+ALTER TABLE `employee_status_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`),
+  ADD KEY `record_number` (`record_number`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD UNIQUE KEY `record_number` (`record_number`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee_status_log`
+--
+ALTER TABLE `employee_status_log`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee_status_log`
+--
+ALTER TABLE `employee_status_log`
+  ADD CONSTRAINT `employee_status_log_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
+  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`record_number`) REFERENCES `tasks` (`record_number`);
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

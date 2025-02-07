7b99e687dc47d2337481309a7bf96561f91aec64
Rod <asperarodalbert@gmail.com>
2025-02-07 11:40:58 +0800

    Update navbar.php

--- MONITORING SYSTEM/include/navbar.php
+++ MONITORING SYSTEM/include/navbar.php
@@ -1,37 +1,69 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark" id="topNavBG">
-    <a class="navbar-brand ps-3" href="index.php">Employee Monitoring</a>
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle"><i class="fas fa-bars"></i></button>
-    <span class="top_nav_space"></span>
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-        <li class="nav-item dropdown">
-            <a class="dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"><i class="fas fa-user fa-fw"></i></a>
-            <ul class="dropdown-menu dropdown-menu-end">
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Activity Log</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Logout</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Side Nav</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <style>
+        .nav-link {
+            border-radius: 10px;
+            margin:10px;
+        }
+        .nav-link:hover {
+            background-color: #0a53be;
+        }
+        .sb-nav-link {
+            margin-right: 10px;
+        }
 
-<div id="layoutSidenav">
-    <div id="layoutSidenav_nav">
-        <nav class="sb-sidenav accordion sb-sidenav-dark">
-            <div class="sb-sidenav-menu">
-                <div class="nav">
-                    <div class="sb-sidenav-menu-heading"><img class="logo" src="img/dasmoLogo.png" alt=""></div>
-                    <br>
-                    <a class="nav-link" id="linkBG" href="dashboard.php"><i class="fas fa-box"></i> Dashboard</a>
-                    <a class="nav-link" href="employees.php"><i class="fas fa-person"></i> Employees</a>
-                    <a class="nav-link" href="tasks.php"><i class="fas fa-clip"></i> Tasks</a>
-                    <a class="nav-link" href="reports.php"><i class="fas fa-chart-line"></i> Reports and Analytics</a>
+        .logo {
+            overflow: hidden;
+            width: 60%;
+        }
+        .sb-sidenav-menu-heading {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body class="sb-nav-fixed">
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading"><img class="logo" src="pictures/dasmoLogo.png" alt=""></div>
+                        <br>
+                        <a class="nav-link" href="index.html">
+                            <div class="sb-nav-link"><i class="fas fa-box"></i></div>
+                            Dashboard
+                        </a>
+    
+                        <a class="nav-link" href="employees.html">
+                            <div class="sb-nav-link"><i class="fas fa-person"></i></div>
+                            Employees
+                        </a>
+    
+                        <a class="nav-link" href="reports.html">
+                            <div class="sb-nav-link"><i class="fas fa-chart-line"></i></div>
+                            Reports and Analytics
+                        </a>
+                        
+                    </div>
                 </div>
-            </div>
-            <div class="sb-sidenav-footer">
-                <div class="small">Logged in as:</div>
-                Admin
-            </div>
-        </nav>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Admin
+                </div>
+            </nav>
+        </div>
     </div>
-</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+
+</body>
+</html>

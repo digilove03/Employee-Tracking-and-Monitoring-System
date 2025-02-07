5cccf7f1868f03ce2387ca885c9ce326304d1ba7
Rod <asperarodalbert@gmail.com>
2025-02-07 13:35:56 +0800

    Update navbar.php

--- MONITORING SYSTEM/include/navbar.php
+++ MONITORING SYSTEM/include/navbar.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Side Nav</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <style>
         .nav-link {
             border-radius: 10px;
@@ -60,10 +59,5 @@
             </nav>
         </div>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
-
 </body>
 </html>

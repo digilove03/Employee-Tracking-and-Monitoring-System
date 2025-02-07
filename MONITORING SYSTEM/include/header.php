22c40effaa68affe3a6a48e21412d3ae2568d352
Rod <asperarodalbert@gmail.com>
2025-02-07 16:00:18 +0800

    Update header.php

--- MONITORING SYSTEM/include/header.php
+++ MONITORING SYSTEM/include/header.php
@@ -5,24 +5,10 @@ if (!isset($_SESSION['admin_id'])) {
     exit();
 }
 ?>
-    
-<?php include 'scripts.php';?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-    <link rel="stylesheet" href="style.css">
-    <style>
-
-    </style>
-</head>
-<body class="sb-nav-fixed">
+  
     <nav class="sb-topnav navbar navbar-expand navbar-dark" id="topNavBG">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.html">Employee Monitoring</a>
+        <a class="navbar-brand ps-3" href="dashboard.php">Employee Monitoring</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
@@ -45,5 +31,3 @@ if (!isset($_SESSION['admin_id'])) {
             </li>
         </ul>
     </nav>
-</body>
-</html>

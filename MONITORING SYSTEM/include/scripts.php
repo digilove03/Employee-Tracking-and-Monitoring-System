77deb890736f4cbf61e8cb1a48d238d286fd0cda
Rod <asperarodalbert@gmail.com>
2025-02-07 13:33:49 +0800

    Update scripts.php

--- MONITORING SYSTEM/include/scripts.php
+++ MONITORING SYSTEM/include/scripts.php
@@ -1,3 +1,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="js/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>

1d69f81330c8514d6995494d2051fa22d09d14ae
Rod <asperarodalbert@gmail.com>
2025-02-07 13:53:54 +0800

    Update dashboard.php

--- MONITORING SYSTEM/dashboard.php
+++ MONITORING SYSTEM/dashboard.php
@@ -1,3 +1,4 @@
+<?php include 'include/scripts.php';?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -9,7 +10,6 @@
         <title>MONITORING</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="css/style.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark" id="topNavBG">
@@ -163,9 +163,5 @@
                 </footer>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-
     </body>
 </html>

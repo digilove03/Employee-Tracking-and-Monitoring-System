2b250a3329c9ca472ef91bc6962ab25eb32eef76
Rod <asperarodalbert@gmail.com>
2025-02-07 15:35:39 +0800

    Update employees.php

--- MONITORING SYSTEM/employees.php
+++ MONITORING SYSTEM/employees.php
@@ -1,3 +1,4 @@
+<?php include 'include/scripts.php';?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,60 +8,15 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>MONITORING SYSTEM</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="css/style.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark" id="topNavBG">
-            <a class="navbar-brand ps-3" href="index.html">Employee Monitoring</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <div class="navbar-brand ps-3"><center>List of Employees</center></div>
-            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-                </div>
-            </form>
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+
+        <div><?php include 'include/header.php';?></div>
+        
         <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading"><img class="logo" src="img/dasmoLogo.png" alt=""></div>
-                            <br>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link"><i class="fas fa-box"></i></div>
-                                Dashboard
-                            </a>
-                            <a class="nav-link" id="linkBG" href="employees.html">
-                                <div class="sb-nav-link"><i class="fas fa-person"></i></div>
-                                Employees
-                            </a>
-                            <a class="nav-link" href="reports.html">
-                                <div class="sb-nav-link"><i class="fas fa-chart-line"></i></div>
-                                Reports and Analytics
-                            </a>
-                        </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Admin
-                    </div>
-                </nav>
-            </div>
+
+            <div><?php include 'include/navbar.php';?></div>
+            
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
@@ -173,10 +129,7 @@
                 document.getElementById('employeeCivilStatus').value = status;
             }
         </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        
+
     </body>
 </html>
 

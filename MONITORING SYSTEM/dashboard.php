d5573bbbf2af714475d52a10c75b0bdf14301778
Rod <asperarodalbert@gmail.com>
2025-02-10 09:35:59 +0800

    Update dashboard.php

--- MONITORING SYSTEM/dashboard.php
+++ MONITORING SYSTEM/dashboard.php
@@ -10,56 +10,15 @@
         <title>MONITORING</title>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark" id="topNavBG">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Employee Monitoring</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <span class="top_nav_space"></span>
-            <!-- Navbar-->
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+        
+        <div><?php include 'include/header.php';?></div>
+        
         <div id="layoutSidenav">
+            
             <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading"><img class="logo" src="img/dasmoLogo.png" alt=""></div>
-                            <br>
-                            <a class="nav-link" id="linkBG" href="index.html">
-                                <div class="sb-nav-link"><i class="fas fa-box"></i></div>
-                                Dashboard
-                            </a>
 
-                            <a class="nav-link" href="employees.html">
-                                <div class="sb-nav-link"><i class="fas fa-person"></i></div>
-                                Employees
-                            </a>
-
-                            <a class="nav-link" href="reports.html">
-                                <div class="sb-nav-link"><i class="fas fa-chart-line"></i></div>
-                                Reports and Analytics
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Admin
-                    </div>
-                </nav>
-            </div>
+                <div><?php include 'include/navbar.php';?></div>
+                
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">

f2a72303318afebb98b90372a1495d48aceb55a4
Rod <asperarodalbert@gmail.com>
2025-02-12 11:05:22 +0800

    Add files via upload

---
+++ MONITORING SYSTEM/tasks.php
@@ -0,0 +1,48 @@
+<?php include 'include/scripts.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>MONITORING</title>
+</head>
+<body class="sb-nav-fixed">
+
+    <div><?php include 'include/header.php';?></div>
+
+    <div id="layoutSidenav">
+        <div><?php include 'include/navbar.php';?></div>
+
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Tasks</h1>
+                    <br>
+
+
+
+
+
+
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; DASMO 2025</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+        </div>
+    </div>
+    
+</body>
+</html>

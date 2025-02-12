1a4581dcff43478c50dfba4af32e18db0ce722be
Rod <asperarodalbert@gmail.com>
2025-02-12 13:38:13 +0800

    Update reports.php

--- MONITORING SYSTEM/reports.php
+++ MONITORING SYSTEM/reports.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>MONITORING</title>
+        <title>EMPLOYEE MONITORING AND TRACKING SYSTEM</title>
         <style>             
             .buttons {
               font-family: Arial, sans-serif;

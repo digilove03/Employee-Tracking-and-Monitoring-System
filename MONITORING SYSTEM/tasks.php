54518a193d3e5bf3fc3f9dd60b1e59c17b9d2cdf
Rod <asperarodalbert@gmail.com>
2025-02-12 13:38:29 +0800

    Update tasks.php

--- MONITORING SYSTEM/tasks.php
+++ MONITORING SYSTEM/tasks.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Task Monitoring</title>
+    <title>EMPLOYEE MONITORING AND TRACKING SYSTEM</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>

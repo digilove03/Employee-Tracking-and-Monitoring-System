d19524214cf8cb84b32bcf3df662c6d980cf4f52
digilove03 <142278349+digilove03@users.noreply.github.com>
2025-02-13 10:45:33 +0800

    Update fetch_employees.php

--- MONITORING SYSTEM/fetch_employees.php
+++ MONITORING SYSTEM/fetch_employees.php
@@ -4,17 +4,29 @@ require 'db_connect.php';
 $query = "SELECT * FROM employee ORDER BY id ASC";
 $result = $conn->query($query);
 
-while ($row = $result->fetch_assoc()): ?>
+while ($row = $result->fetch_assoc()): 
+    // Check if photo exists, otherwise use a default image
+    $photoPath = !empty($row['photo_path']) ? htmlspecialchars($row['photo_path']) : 'emp_profile/default.png';
+?>
     <div class="col-xl-3 col-md-6 employee-card">
         <div class="card total_employee text-white bg-primary mb-4">
             <div class="card-body">
-                <i class="fas fa-id-badge"></i>
+                <center>
+                    <img src="<?php echo $photoPath; ?>" 
+                         alt="Profile Picture" 
+                         class="img-fluid rounded-circle" 
+                         style="width: 80px; height: 80px; object-fit: cover;">
+                </center>
             </div>
             <div class="card-footer">
-                <center><h4><b><?php echo explode(' ', htmlspecialchars($row['first_name']))[0]; ?></b></h4></center>
+                <center><h4><b><?php echo htmlspecialchars(explode(' ', $row['first_name'])[0]); ?></b></h4></center>
                 <div class="row mt-2">
-                    <a href="#?id=<?php echo $row['id']; ?>" class="btn btn-success col"><i class="fas fa-pencil"></i></a>
-                    <a href="#?id=<?php echo $row['id']; ?>" class="btn btn-danger col" onclick="return confirm('Are you sure?');"><i class="fas fa-trash"></i></a>
+                    <a href="edit_employee.php?id=<?php echo $row['id']; ?>" class="btn btn-success col">
+                        <i class="fas fa-pencil"></i>
+                    </a>
+                    <a href="delete_employee.php?id=<?php echo $row['id']; ?>" class="btn btn-danger col" onclick="return confirm('Are you sure?');">
+                        <i class="fas fa-trash"></i>
+                    </a>
                 </div>
             </div>
         </div>

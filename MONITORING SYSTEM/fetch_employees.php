0dabe4f7509b34c8711a43adf4b1489d60c91a1f
digilove03 <142278349+digilove03@users.noreply.github.com>
2025-02-20 11:01:37 +0800

    Update fetch_employees.php

--- MONITORING SYSTEM/fetch_employees.php
+++ MONITORING SYSTEM/fetch_employees.php
@@ -1,17 +1,19 @@
 <?php
 require 'db_connect.php';
 
-$query = "SELECT * FROM employee ORDER BY id ASC";
+// Combine your queries into one query that computes the age dynamically.
+$query = "SELECT *, TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) AS age FROM employee ORDER BY id ASC";
 $result = $conn->query($query);
 
-while ($row = $result->fetch_assoc()): 
-    // Check if photo exists, otherwise use a default image
+while ($row = $result->fetch_assoc()):
+    // Check if photo exists; otherwise, use a default image.
     $photoPath = !empty($row['photo_path']) ? htmlspecialchars($row['photo_path']) : 'emp_profile/default.png';
 ?>
     <div class="col-xl-3 col-md-6 employee-card">
         <div class="card total_employee text-white bg-primary mb-4">
             <div class="card-body">
                 <center>
+                    <!-- Use $photoPath here -->
                     <img src="<?php echo $photoPath; ?>" 
                          alt="Profile Picture" 
                          class="img-fluid rounded-circle" 
@@ -19,7 +21,9 @@ while ($row = $result->fetch_assoc()):
                 </center>
             </div>
             <div class="card-footer">
-                <center><h4><b><?php echo htmlspecialchars(explode(' ', $row['first_name'])[0]); ?></b></h4></center>
+                <center>
+                    <h4><b><?php echo htmlspecialchars(explode(' ', $row['first_name'])[0]); ?></b></h4>
+                </center>
                 <div class="row mt-2">
                     <a href="edit_employee.php?id=<?php echo $row['id']; ?>" class="btn btn-success col">
                         <i class="fas fa-pencil"></i>

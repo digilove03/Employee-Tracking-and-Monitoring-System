9a87d48643800751f1073afd4ab1b57b1c3d3a30
digilove03 <142278349+digilove03@users.noreply.github.com>
2025-02-28 15:10:03 +0800

    Update save_employees.php

--- MONITORING SYSTEM/save_employees.php
+++ MONITORING SYSTEM/save_employees.php
@@ -22,6 +22,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email         = htmlspecialchars(trim($_POST['email']));
     $religion      = htmlspecialchars(trim($_POST['religion'])); 
 
+    $status = "Available";
+
+    // Check if the employee already exists based on name and birthdate
+    $checkEmployeeQuery = "SELECT id FROM employee WHERE last_name = ? AND first_name = ? AND middle_name = ? AND suffix = ? AND birthdate =?";
+    $checkEmployeeStmt = $conn->prepare($checkEmployeeQuery);
+    $checkEmployeeStmt->bind_param("sssss", $lastName, $firstName, $middleName, $suffix, $birthdate);
+    $checkEmployeeStmt->execute();
+    $checkEmployeeStmt->store_result();
+
+    if ($checkEmployeeStmt->num_rows > 0) {
+        $response['error'] = "An employee with the same name and birthdate already exists.";
+        echo json_encode($response);
+        exit;
+    }
+    $checkEmployeeStmt->close();
+    
     // Check if the email already exists
     $checkQuery = "SELECT id FROM employee WHERE email_address = ?";
     $checkStmt = $conn->prepare($checkQuery);
@@ -59,14 +75,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Prepare SQL query
-    $stmt = $conn->prepare("INSERT INTO employee (first_name, last_name, middle_name, suffix, sex, birthdate, address, position, department, civil_status, hiring_date, contact_number, email_address, photo_path, religion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO employee (first_name, last_name, middle_name, suffix, sex, birthdate, address, position, department, civil_status, hiring_date, contact_number, email_address, photo_path, religion, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
     if (!$stmt) {
         $response['error'] = "Prepare failed: " . $conn->error;
         echo json_encode($response);
         exit;
     }
     
-    $stmt->bind_param("sssssssssssisss", 
+    $stmt->bind_param("sssssssssssissss", 
         $firstName, 
         $lastName, 
         $middleName, 
@@ -81,7 +97,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $contactNumber, 
         $email, 
         $photoPath, 
-        $religion
+        $religion,
+        $status
     );
 
     // Execute and check for success
